5ced91a8d488cd3433caf66775b8e267da1b3d25
safiulm123 <safiulhasan@gmail.com>
2019-03-15 01:11:03 +0500

    Update server.js
    Updated some comments for better understanding

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const expressGraphQL = require('express-graphql')
 const schema = require('./schema')
 const app = express();
 
-app.use('/graphql', expressGraphQL({ // Over herer the its "/graphql" and not "./graphql"
+app.use('/graphql', expressGraphQL({ // Over here its "/graphql" and not "./graphql"
     schema:schema,
     graphiql:true // make sure that this is "graphiql" rather than "graphql"
 }))

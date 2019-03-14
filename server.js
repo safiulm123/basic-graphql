2c22d1964eb0037cfde8c88e32f1d0e32a52b4c7
Safiul Hasan <safiulhasan@gmail.com>
2019-03-14 13:41:32 +0500

    First Commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const expressGraphQL = require('express-graphql')
+const schema = require('./schema')
+const app = express();
+
+app.use('/graphql', expressGraphQL({ // Over herer the its "/graphql" and not "./graphql"
+    schema:schema,
+    graphiql:true // make sure that this is "graphiql" rather than "graphql"
+}))
+app.listen(4000,()=>{
+    console.log("Server is running on port 4000");
+})
